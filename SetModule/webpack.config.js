8066819518dba75415e184dc89a9be2f7e2653ed
yamada <shinya.yamada@benext-corp.com>
2023-02-26 16:21:07 +0900

    🔧 update setting to webpack v5

--- SetModule/webpack.config.js
+++ SetModule/webpack.config.js
@@ -1,12 +1,26 @@
 const path = require("path");
 
 module.exports = {
+  mode: "development",
   entry: "./src/app.ts",
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
+    publicPath: "/dist",
   },
-  devtool: "inline-source-map",
+  devServer: {
+    static: [
+      {
+        directory: path.resolve(__dirname, "dist"),
+        publicPath: "/dist",
+      },
+      {
+        directory: __dirname,
+        publicPath: "/",
+      },
+    ],
+  },
+  devtool: "eval",
   module: {
     rules: [
       {
