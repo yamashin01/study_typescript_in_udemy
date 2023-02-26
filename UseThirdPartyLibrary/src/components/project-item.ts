02fae16e3d164966e1c3fbd723cca142f7f1587b
yamada <shinya.yamada@benext-corp.com>
2023-02-27 07:25:47 +0900

    begin project thirdparty library

---
+++ UseThirdPartyLibrary/src/components/project-item.ts
@@ -0,0 +1,46 @@
+import { Draggable } from "../models/drag-drop";
+import Component from "./base-component";
+import { Project } from "../models/project";
+import { autobind } from "../decolators/autobind";
+
+export class ProjectItem
+  extends Component<HTMLUListElement, HTMLLIElement>
+  implements Draggable
+{
+  private project: Project;
+
+  get manday() {
+    if (this.project.manday < 20) {
+      return this.project.manday.toString() + "人日";
+    } else {
+      return (this.project.manday / 20).toString() + "人月";
+    }
+  }
+
+  constructor(hostId: string, project: Project) {
+    super("single-project", hostId, false, project.id);
+    this.project = project;
+
+    this.configure();
+    this.renderContent();
+  }
+
+  @autobind
+  dragStartHandler(e: DragEvent): void {
+    e.dataTransfer!.setData("text/plain", this.project.id);
+    e.dataTransfer!.effectAllowed = "move";
+  }
+  dragEndHandler(e: DragEvent): void {
+    console.log(e, "Drag終了");
+  }
+
+  configure(): void {
+    this.element.addEventListener("dragstart", this.dragStartHandler);
+    this.element.addEventListener("dragend", this.dragEndHandler);
+  }
+  renderContent(): void {
+    this.element.querySelector("h2")!.textContent = this.project.title;
+    this.element.querySelector("h3")!.textContent = this.manday;
+    this.element.querySelector("p")!.textContent = this.project.description;
+  }
+}
