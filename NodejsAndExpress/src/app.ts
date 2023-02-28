18a5c22dafa51af372121d660f1e8b8d5d3b57cf
yamada <shinya.yamada@benext-corp.com>
2023-02-28 18:36:58 +0900

    ✨ add CRUD process

--- NodejsAndExpress/src/app.ts
+++ NodejsAndExpress/src/app.ts
@@ -1,5 +1,12 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import todoRoutes from "./routes/todos";
 
 const app = express();
+
+app.use("/todos", todoRoutes);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  res.status(500).json({ message: err.message });
+});
 
 app.listen(3000);
